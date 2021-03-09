19d4c7e9f0189484837a5c2a15b481dc3892026f
Clint <maxeva02@gmail.com>
2021-03-08 21:42:08 -0800

    setting up eslint

--- tic-tac-toe/tac.js
+++ tic-tac-toe/tac.js
@@ -1,7 +1,6 @@
 const statusDiv = document.querySelector('.status');
 const resetDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
-
 // game constants
 const xSymbol = '×';
 const oSymbol = '○';
